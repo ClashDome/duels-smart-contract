39da0ee289440ef40c0f44848e50736d2a2337ea
David Escriche Oro <davidescriche6@gmail.com>
2021-03-05 11:31:12 +0100

    ELO system implementation

--- clashdomedls.cpp
+++ clashdomedls.cpp
@@ -1,37 +1,108 @@
 #include "clashdomedls.hpp"
+#include <math.h>
 
-void clashdomedls::createduel(string state, string type, string game, asset fee)
+void clashdomedls::create(uint64_t id, uint64_t type, uint64_t game, asset fee, uint64_t duration, string data)
 {
     require_auth(_self);
 
     duels _dl(CONTRACTN, CONTRACTN.value);
 
+    auto dl_itr = _dl.find(id);
+    check(dl_itr == _dl.end(), "Duel with id " + to_string(id) + " already exists!");
+
     uint64_t timestamp = eosio::current_time_point().sec_since_epoch();
 
+    player_duel player;
+
+    player.timestamp = timestamp;
+    player.duration = duration;
+    player.data = data;
+
     _dl.emplace(CONTRACTN, [&](auto& new_duel) {
-        new_duel.id = _dl.available_primary_key();
+        new_duel.id = id;
         new_duel.timestamp = timestamp;
-        new_duel.state = state;
+        new_duel.state = DuelState::OPEN;
         new_duel.type = type;
         new_duel.game = game;
         new_duel.fee = fee;
+        new_duel.player1 = player;
     });
 }
 
-void clashdomedls::updateelo(string game, name winner, name loser)
+void clashdomedls::compromise(uint64_t id, name account)
 {
     require_auth(_self);
 
+    duels _dl(CONTRACTN, CONTRACTN.value);
+
+    auto dl_itr = _dl.find(id);
+    check(dl_itr != _dl.end(), "Duel with id " + to_string(id) + " doesn't exists!");
+    check(dl_itr->state != DuelState::COMPROMISED, "Duel with id " + to_string(id) + " already compromised!");
+
+    uint64_t timestamp = eosio::current_time_point().sec_since_epoch();
+    
+    player_duel player;
+
+    player.account = account;
+    player.timestamp = timestamp;
+
+    _dl.modify(dl_itr, get_self(), [&](auto &mod_duel) {
+        mod_duel.state = DuelState::COMPROMISED;
+        mod_duel.player2 = player;
+    });
+}
+
+void clashdomedls::close(uint64_t id, name account, uint64_t score, uint64_t duration, uint64_t score2, name account2)
+{
+    require_auth(_self);
+
+    duels _dl(CONTRACTN, CONTRACTN.value);
+
+    auto dl_itr = _dl.find(id);
+    check(dl_itr != _dl.end(), "Duel with id " + to_string(id) + " doesn't exists!");
+    check(dl_itr->state != DuelState::CLOSED, "Duel with id " + to_string(id) + " already closed!");
+    check(dl_itr->player2.account == account, "Second player account mismatch!");
+
+    _dl.modify(dl_itr, get_self(), [&](auto &mod_duel) {
+        mod_duel.state = DuelState::CLOSED;
+        mod_duel.player1.score = score2;
+        mod_duel.player1.account = account2;
+        mod_duel.player2.score = score;
+        mod_duel.player2.duration = score;
+    });
+
+    // update elo
+
+    name winner;
+    name loser;
+    uint64_t game = dl_itr->game;
+
+    if (dl_itr->player1.score > dl_itr->player2.score) {
+        winner = dl_itr->player1.account;
+        loser = dl_itr->player2.account;
+    } else if (dl_itr->player2.score > dl_itr->player1.score) {
+        winner = dl_itr->player2.account;
+        loser = dl_itr->player1.account;
+    } else {
+        if (dl_itr->player1.duration <= dl_itr->player2.duration) {
+            winner = dl_itr->player1.account;
+            loser = dl_itr->player2.account;
+        } else {
+            winner = dl_itr->player2.account;
+            loser = dl_itr->player1.account;
+        }
+    }
+
     players _pl(CONTRACTN, CONTRACTN.value);
 
-    // WINNER
+    // winner
     auto pl_itr = _pl.find(winner.value);
 
     // new user
     if (pl_itr == _pl.end()) {
 
         vector<game_info> games;
-        games.push_back({ game, 1, 1, 0, 1000 }); 
+        games.push_back({ game, 1, 1, 0, 1000}); 
 
         _pl.emplace(CONTRACTN, [&](auto& new_player) {
             new_player.account = winner;
@@ -53,9 +124,138 @@ void clashdomedls::updateelo(string game, name winner, name loser)
             });
         }
     }
+
+    // loser
+    auto pl_itr_2 = _pl.find(loser.value);
+
+    // new user
+    if (pl_itr_2 == _pl.end()) {
+
+        vector<game_info> games;
+        games.push_back({ game, 1, 0, 1, 1000}); 
+
+        _pl.emplace(CONTRACTN, [&](auto& new_player) {
+            new_player.account = loser;
+            new_player.games = games;
+        });
+    } else {
+
+        uint64_t pos = finder(pl_itr_2->games, game);
+
+        // new game
+        if (pos == -1) {
+            _pl.modify(pl_itr_2, get_self(), [&](auto &mod_player) {
+                mod_player.games.push_back({ game, 1, 0, 1, 1000 });
+            });
+        } else {
+            _pl.modify(pl_itr_2, get_self(), [&](auto &mod_player) {
+                mod_player.games.at(pos).total_duels++;
+                mod_player.games.at(pos).loses++;
+            });
+        }
+    }
+
+    pl_itr = _pl.find(winner.value);
+    uint64_t pos = finder(pl_itr->games, game);
+    uint64_t winnerMMR = pl_itr->games.at(pos).MMR;
+
+    pl_itr_2 = _pl.find(loser.value);
+    pos = finder(pl_itr_2->games, game);
+    uint64_t loserMMR = pl_itr_2->games.at(pos).MMR;
+
+
+    float p1 = 1.0 * 1.0 / (1.0 + 1.0 * pow(10, 1.0 * (loserMMR - winnerMMR) / 400)); 
+    float p2 = 1.0 * 1.0 / (1.0 + 1.0 * pow(10, 1.0 * (winnerMMR - loserMMR) / 400)); 
+    uint64_t K = 30;
+
+    winnerMMR = winnerMMR + K * (1.0 - p1);
+    loserMMR = loserMMR + K * (0.0 - p2);
+
+    _pl.modify(pl_itr, get_self(), [&](auto &mod_player) {
+        mod_player.games.at(pos).MMR = winnerMMR;
+    });
+
+    _pl.modify(pl_itr_2, get_self(), [&](auto &mod_player) {
+        mod_player.games.at(pos).MMR = loserMMR;
+    });
 }
 
-void clashdomedls::eraseall() {
+void clashdomedls::claim(uint64_t id, name account)
+{
+
+    require_auth(account);
+
+    duels _dl(CONTRACTN, CONTRACTN.value);
+
+    auto dl_itr = _dl.find(id);
+    check(dl_itr != _dl.end(), "Duel with id " + to_string(id) + " doesn't exists!");
+    check(dl_itr->state != DuelState::CLAIMED, "Duel with id " + to_string(id) + " already claimed!");
+    check(dl_itr->state == DuelState::CLOSED, "Duel with id " + to_string(id) + " can't be claimed yet!");
+
+    name winner;
+
+    if (dl_itr->player1.score > dl_itr->player2.score) {
+        winner = dl_itr->player1.account;
+    } else if (dl_itr->player2.score > dl_itr->player1.score) {
+        winner = dl_itr->player2.account;
+    } else {
+        if (dl_itr->player1.duration <= dl_itr->player2.duration) {
+            winner = dl_itr->player1.account;
+        } else {
+            winner = dl_itr->player2.account;
+        }
+    }
+
+    check(winner == account, "Player with account " + account.to_string() + " isn't the winner!");
+
+    _dl.modify(dl_itr, get_self(), [&](auto &mod_duel) {
+        mod_duel.state = DuelState::CLAIMED;
+    });
+
+    string game = "";
+
+    if (dl_itr->game == GameType::ENDLESS_SIEGE) {
+        game = "Endless Siege";
+    } else if (dl_itr->game == GameType::CANDY_FIESTA) {
+        game = "Candy Fiesta";
+    }
+
+    action(permission_level{_self, "active"_n}, EOS_CONTRACT, "transfer"_n, make_tuple(_self, account, dl_itr->fee * 2 * 95 / 100, string(game + ". Duel id " + to_string(id) + " - Winner"))).send(); 
+    action(permission_level{_self, "active"_n}, EOS_CONTRACT, "transfer"_n, make_tuple(_self, COMPANY_ACCOUNT, dl_itr->fee * 2 * 5 / 100, string(game + ". Duel id " + to_string(id) + " - Commission"))).send();   
+}
+
+void clashdomedls::reopen(uint64_t id)
+{
+    require_auth(_self);
+
+    duels _dl(CONTRACTN, CONTRACTN.value);
+
+    auto dl_itr = _dl.find(id);
+    check(dl_itr != _dl.end(), "Duel with id " + to_string(id) + " doesn't exists!");
+    check(dl_itr->state == DuelState::COMPROMISED, "Duel with id " + to_string(id) + " can't be reopened!");
+
+    player_duel player;
+
+    player.timestamp = 0;
+
+    _dl.modify(dl_itr, get_self(), [&](auto &mod_duel) {
+        mod_duel.state = DuelState::OPEN;
+        mod_duel.player2 = player;
+    });
+}
+
+void clashdomedls::remove(uint64_t id)
+{
+    require_auth(_self);
+
+    duels _dl(CONTRACTN, CONTRACTN.value);
+
+    auto dl_itr = _dl.find(id);
+    check(dl_itr != _dl.end(), "Duel with id " + to_string(id) + " doesn't exists!");
+    _dl.erase(dl_itr);
+}
+
+void clashdomedls::revomeall() {
 
     require_auth(_self);
 
@@ -70,7 +270,7 @@ void clashdomedls::eraseall() {
     }
 }
 
-uint64_t clashdomedls::finder(vector<game_info> games, string id)
+uint64_t clashdomedls::finder(vector<game_info> games, uint64_t id)
 {
     for (uint64_t i = 0; i < games.size(); i++)
     {
--- clashdomedls.hpp
+++ clashdomedls.hpp
@@ -14,6 +14,8 @@ using eosio::current_time_point;
 using eosio::action;
 using eosio::same_payer;
 using eosio::multi_index;
+using eosio::indexed_by;
+using eosio::const_mem_fun;
 using std::string;
 using std::vector;
 using std::make_tuple;
@@ -27,21 +29,27 @@ class [[eosio::contract]] clashdomedls : public eosio::contract
 
         using contract::contract;
 
-        ACTION createduel(string state, string type, string game, asset fee);
-        ACTION updateelo(string game, name winner, name loser);
-        ACTION eraseall();
+        ACTION create(uint64_t id, uint64_t type, uint64_t game, asset fee, uint64_t duration, string data);
+        ACTION compromise(uint64_t id, name account);
+        ACTION close(uint64_t id, name account, uint64_t score, uint64_t duration, uint64_t score2, name account2);
+        ACTION claim(uint64_t id, name account);
+        ACTION reopen(uint64_t id);
+        ACTION remove(uint64_t id);
+        ACTION revomeall();
 
     private:
 
         struct player_duel
         {
             name account;   // user wax account
-            uint64_t initial_timestamp;
+            uint64_t timestamp;
+            uint64_t duration;
             uint64_t score; // user score
+            string data;
         };
 
         struct game_info {
-            string id; 
+            uint64_t id; 
             uint64_t total_duels;  
             uint64_t wins;
             uint64_t loses;
@@ -58,18 +66,34 @@ class [[eosio::contract]] clashdomedls : public eosio::contract
 
         TABLE duel {
             uint64_t id;
-            uint64_t timestamp; // duel creation time
-            string type;  // public || private
-            string state;  // open || compromised || closed || claimed
-            string game; // endless-siege, candy-fiesta, etc
-            asset fee; // entry fee for the duel
+            uint64_t timestamp;
+            uint64_t type;          // public 0 || private 1
+            uint64_t state;         // open 0 || compromised 1 || closed 2 || claimed 3
+            uint64_t game;          // endless-siege 0 || candy-fiesta 1 || ...
+            asset fee;              // entry fee for the duel
             player_duel player1;
             player_duel player2;
 
             uint64_t primary_key() const { return id; }
+            uint64_t getDuel() const { return (game << 16) | state; }
+            uint64_t getFirst() const { return player1.account.value; }
+            uint64_t getSecond() const { return player2.account.value; }
+            
         };
 
-        typedef multi_index<name("duels"), duel> duels;
+        typedef multi_index<name("duels"), duel, 
+            indexed_by<name("getduel"), const_mem_fun<duel, uint64_t, &duel::getDuel>>,
+            indexed_by<name("getfirst"), const_mem_fun<duel, uint64_t, &duel::getFirst>>,
+            indexed_by<name("getsecond"), const_mem_fun<duel, uint64_t, &duel::getSecond>>>
+        duels;
 
-        uint64_t finder(vector<game_info> games, string id);
+        enum DuelState {OPEN = 0, COMPROMISED, CLOSED, CLAIMED};
+        enum GameType {ENDLESS_SIEGE = 0, CANDY_FIESTA};
+
+
+        static constexpr name COMPANY_ACCOUNT = "gr.au.wam"_n;
+        static constexpr name EOS_CONTRACT = "eosio.token"_n;
+        static constexpr symbol WAX_SYMBOL = symbol(symbol_code("WAX"), 8);
+
+        uint64_t finder(vector<game_info> games, uint64_t id);
 };
